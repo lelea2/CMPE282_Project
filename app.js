a33f2e429b5269405f8b2f9a70fa987d1f548722
Khanh Dao <kdao@Khanhs-MBP-2.attlocal.net>
2016-05-13 06:09:23 -0700

    Initial check in for integrate with azrue

--- app.js
+++ app.js
@@ -1,32 +1,76 @@
-/**
- * Module dependencies.
- */
-var express     = require('express');
+var express  = require('express'),
+    compress = require('compression'),
+    hbs      = require('hbs'),
+    moment   = require('moment'),
+    router   = require(__dirname + '/routes').router,
+    app      = express(),
+    error    = require(__dirname + '/middleware/error'),
+    logger = require('connect-logger'),
+    cookieParser = require('cookie-parser'),
+    session = require('cookie-session'),
+    fs = require('fs'),
+    crypto = require('crypto');
 
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
+hbs.registerPartials(__dirname + '/views/partials');
 
-// Load Configurations
+hbs.registerHelper('dateFormat', function(context, block) {
+    var f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
+    return moment(context).format(f);
+});
 
-var env             = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var config          = require('./config/config');
-var db              = require('./config/sequelize');
-var passport        = require('./config/passport');
-var winston         = require('./config/winston');
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/pages');
+app.engine('html', hbs.__express);
 
-// console.log(db);
+app.use(logger());
+app.use(cookieParser('a deep secret'));
+app.use(session({secret: '1234567890QWERTY'}));
 
-var app = express();
+app.use(compress({
+    filter: function(req, res) {
+        return (/json|text|javascript|css|image\/svg\+xml|application\/x-font-ttf/).test(res.getHeader('Content-Type'));
+    },
+    level: 9
+}));
 
-//Initialize Express
-require('./config/express')(app, passport, db);
+if (app.get('env') === 'development'){
+    app.use(express.static(__dirname + '/public', {maxAge: 86400000}));
+}
 
-//Start the app by listening on <port>
-var port = process.env.PORT || config.port;
-app.listen(port);
-winston.info('Express app started on port ' + port);
+var route = express.Router();
 
-//expose app
-exports = module.exports = app;
+route.get('/index.html', function(req, res){
+    res.redirect(301, '/');
+});
+route.get('/', router.index);
+route.get('/auth', router.auth);
+route.get('/getAToken', router.getAToken);
+
+
+app.use('/', route);
+
+app.use(error.notFound);
+app.use(error.serverError);
+
+/** Start App **/
+var cluster = require('cluster'),
+    numCPUs = require('os').cpus().length,
+    app,
+    server;
+if (cluster.isMaster && !module.parent) {
+    for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', function(worker, code, signal) {
+      console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    server = app.listen(process.env.PORT || 3000, '127.0.0.1', function () {
+        var host = server.address().address,
+            port = server.address().port;
+        console.log('App listening at http://%s:%s with worker id %s', host, port, cluster.worker.id);
+    });
+}
+/** End handling Start App **/
+
+module.exports = app;
--- gruntfile.js
+++ gruntfile.js
@@ -1,155 +1,77 @@
-'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-module.exports = function(grunt) {
+    var date = (new Date()).valueOf().toString();
 
-        var paths = {
-        js: ['!bower_components/**',  'packages/**/*.js' , 'packages/**/**/*.js', '!packages/**/assets/**'],
-        jsPublic:['packages/*/public/*.js','packages/*/public/**/*.js'],
-        jsPublicAnnotate:['packages/*/public/*annotated.js','packages/*/public/**/*.annotated.js'],
-        jade: ['app/views/**'],
-        html:['packages/views/**'],
-        css: ['!bower_components/**','packages/**/*.css']
-      };
+    var paths = {
+        js: ['public/assets/js/app.js'],
+        css: ['public/assets/css/style.css']
+    };
 
-
-    // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        assets: grunt.file.readJSON('config/assets.json'),
-        clean: ['bower_components/build', 'packages/bundle','packages/**/public/**/*annotated.js'],
-        watch: {
-            jade: {
-                files: paths.jade,
-                options: {
-                    livereload: true,
-                },
+        assemble: {
+            options: {
+                layout: "",
+                flatten: true,
+                helpers: ['./helpers/*.js'],
+                partials: ['views/partials/*.hbs'],
+                data: './routes/seo.json'
             },
-            js: {
-                files: paths.js,
-                tasks: ['jshint'],
-                options: {
-                    livereload: true,
-                },
-            },
-            html: {
-                files: paths.html,
-                options: {
-                    livereload: true,
-                },
-            },
-            css: {
-                files: paths.css,
-                tasks: ['csslint'],
-                options: {
-                    livereload: true
+            pages: {
+                files: {
+                    'build/': ['views/pages/*.html']
                 }
             }
         },
-        ngAnnotate:{
-          options: {
-             singleQuotes: true,
-         },
-         app: {
-           files: [
-                {
+        clean: {
+            all: ['build/**/*.*', '!.htaccess']
+        },
+        copy: {
+            main: {
+                files: [{
                     expand: true,
-                    src: paths.jsPublic,
-                    ext: '.annotated.js', // Dest filepaths will have this extension.
-                    extDot: 'last',       // Extensions in filenames begin after the last dot
-                }
-            ]
-          }
+                    cwd: 'public/',
+                    src: ['**'],
+                    dest: 'build/'
+                }]
+            }
+        },
+        replace: {
+            build_replace: {
+                options: {
+                    patterns: [{
+                        match: /(href|src)="([^"]+)"/g,
+                        replacement: function (href) {
+                            return href.replace(/(\.css|\.ico|\.js|\.jpg|\.png|\.gif|\.pdf)/, '.' + date + '$1');
+                        }
+                    }]
+                },
+                files: [{
+                    src: ['build/*'],
+                    dest: './'
+                }]
+            }
         },
         jshint: {
-          options: {
-                   jshintrc:true // relative to Gruntfile
-                },
+            options: {
+               jshintrc:true // relative to Gruntfile
+            },
             all: paths.js
         },
-        uglify: {
-         core: {
-              options: {
-                 compress: true,
-                 mangle: false
-               },
-             files: '<%= assets.core.js %>'
-           },
-          target: {
-               options: {
-                 compress: true,
-                 quoteStyle:1
-                },
-              files: {'packages/bundle/bundle.min.js': paths.jsPublicAnnotate}
-            },
-
-       },
-       csslint: {
-           options: {
-               csslintrc: '.csslintrc'
-           },
-           src: paths.css
-       },
-       cssmin: {
-           core: {
-               files: '<%= assets.core.css %>'
-             },
-           target:{
-                  files: {'packages/bundle/bundle.min.css': paths.css}
-            }
-      },
-        nodemon: {
-            dev: {
-                script: 'app.js',
-                options: {
-                    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
-                    ext: 'js',
-                    watch: ['packages', 'config'],
-                    delayTime: 1,
-                    env: {
-                        PORT: 3000
-                    },
-                    cwd: __dirname
-                }
-            }
-        },
-        concurrent: {
-            tasks: ['nodemon', 'watch'],
+        csslint: {
             options: {
-                logConcurrentOutput: true
-            }
-        },
-        mochaTest: {
-            options: {
-                reporter: 'spec'
+                csslintrc: '.csslintrc'
             },
-            src: ['test/mocha/**/*.js']
-        },
-        env: {
-            test: {
-                NODE_ENV: 'test'
-            }
-        },
-        karma: {
-            unit: {
-                configFile: 'test/karma/karma.conf.js'
-            }
+            src: paths.css
         }
     });
-
-    //Load NPM tasks
     require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    //Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
-    //Default task(s).
-    if (process.env.NODE_ENV === 'production') {
-        grunt.registerTask('default', ['clean','ngAnnotate','uglify','cssmin', 'concurrent']);
-    } else {
-        grunt.registerTask('default', ['clean','csslint','jshint', 'concurrent']);
-    }
-    grunt.registerTask('lint', ['clean','csslint','jshint', 'uglify', 'cssmin']);
-    grunt.registerTask('annotate',['clean','ngAnnotate']);
-
-    //Test task.
-    grunt.registerTask('test', ['env:test', 'mochaTest'/*, 'karma:unit'*/]);
+    grunt.registerTask('build', ['clean', 'assemble', 'copy', 'replace']);
+    grunt.registerTask('lint', ['csslint','jshint', 'clean', 'assemble', 'copy', 'replace']);
 };
