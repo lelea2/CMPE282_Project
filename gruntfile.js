ff27667917ced2835d00ceb91d001a4890c50fcb
Khanh Dao <kdao@Khanhs-MacBook-Pro-2.local>
2016-05-13 17:28:23 -0700

    Jenkins build should fail

--- gruntfile.js
+++ gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
     });
     //require('load-grunt-tasks')(grunt);
     //Load NPM tasks
-    require('load-grunt-tasks')(grunt);
+    //require('load-grunt-tasks')(grunt);
 
     //Making grunt default to force in order not to break the project.
     //grunt.option('force', true);
