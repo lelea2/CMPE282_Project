175d0b794239ccd82a302d7ca46958adf56f096a
Khanh Dao <kdao@Khanhs-MacBook-Pro-2.local>
2016-05-13 17:29:15 -0700

    Build should be successful

--- gruntfile.js
+++ gruntfile.js
@@ -68,10 +68,11 @@ module.exports = function (grunt) {
     });
     //require('load-grunt-tasks')(grunt);
     //Load NPM tasks
-    //require('load-grunt-tasks')(grunt);
+    //Comment this out for fail build
+    require('load-grunt-tasks')(grunt);
 
     //Making grunt default to force in order not to break the project.
-    //grunt.option('force', true);
+    grunt.option('force', true);
     grunt.loadNpmTasks('assemble');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-clean');
